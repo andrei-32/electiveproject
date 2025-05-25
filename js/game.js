5be637f080a1a7e684efd96e76f132126afba2d0
Trplnr <87922281+Trioplane@users.noreply.github.com>
2025-05-25 14:23:15 +0800

    try fix

--- js/game.js
+++ js/game.js
@@ -9,14 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const roomIdSpan = document.getElementById('roomId');
     const gameStatus = document.getElementById('gameStatus');
     const choiceButtons = [
-        document.getElementById('#rockButton'),
-        document.getElementById('#paperButton'),
-        document.getElementById('#scissorsButton'),
+        document.getElementById('rockButton'),
+        document.getElementById('paperButton'),
+        document.getElementById('scissorsButton'),
     ];
     const opponentChoices = [
-        document.getElementById('#rockChoice'),
-        document.getElementById('#paperChoice'),
-        document.getElementById('#scissorsChoice')
+        document.getElementById('rockChoice'),
+        document.getElementById('paperChoice'),
+        document.getElementById('scissorsChoice')
     ]
     // const playerChoice = document.getElementById('playerChoice');
     // const opponentChoice = document.getElementById('opponentChoice');
@@ -159,8 +159,6 @@ document.addEventListener('DOMContentLoaded', () => {
             const shouldDisable = gameState.roundComplete || 
                                 gameState.gameComplete || 
                                 gameState.playerChoice !== null;
-
-            console.log(button)
             
             button.disabled = shouldDisable;
             
